07f2f9e3e02906a74f725e9848b9d12d95d64717
Firtalion <oliviermael@gmail.com>
2020-09-30 10:02:07 +0200

    Ajoutez des fichiers projet.

---
+++ WebApplication5/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication5.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(new Models.Identifiants());
+        }
+        [HttpPost]
+        public ActionResult index (Models.Identifiants Idts)
+        {
+            
+            if (Idts.UserName =="toto" && Idts.Passwd=="azerty" )
+            {
+            }
+            else
+            {
+                Idts.Message = "Erreur d'identification";
+                Idts.Passwd = string.Empty;
+
+            }
+            
+            return View(Idts);
+        }
+    }
+}
---
+++ WebApplication5/Models/Identifiants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication5.Models
+{
+    public class Identifiants
+    {
+        //attention, propriétés automatique !!!!
+        public string UserName { get; set; }
+        public string Passwd { get; set; }
+        public string Message { get; set; }
+    }
+}
